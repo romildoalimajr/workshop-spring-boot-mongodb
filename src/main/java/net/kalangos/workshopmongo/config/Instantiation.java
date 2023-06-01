f60411f7ea79d3b510cb73d0b98f76513a52ffc6
Romldo A. Lima Jr <odlimor.sevla@gmail.com>
2023-06-01 17:32:37 -0300

    Criando entity Post com User aninhado

--- src/main/java/net/kalangos/workshopmongo/config/Instantiation.java
+++ src/main/java/net/kalangos/workshopmongo/config/Instantiation.java
@@ -1,12 +1,16 @@
 package net.kalangos.workshopmongo.config;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
+import java.util.TimeZone;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import net.kalangos.workshopmongo.domain.Post;
 import net.kalangos.workshopmongo.domain.User;
+import net.kalangos.workshopmongo.repository.PostRepository;
 import net.kalangos.workshopmongo.repository.UserRepository;
 
 @Configuration
@@ -14,12 +18,19 @@ public class Instantiation implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	
+	@Autowired
+	private PostRepository postRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		
 		userRepository.deleteAll();
+		postRepository.deleteAll();
 		
 		User maria = new User(null, "Maria Brown", "maria@kalangos.net");
 		User alex = new User(null, "Alex Green", "alex@kalangos.net");
@@ -27,6 +38,11 @@ public class Instantiation implements CommandLineRunner{
 		User romildo = new User(null, "Romildo Alves", "romildo@kalangos.net");
 		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob, romildo));
+		
+		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
+		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", maria);
+		
+		postRepository.saveAll(Arrays.asList(post1, post2));
 		
 	}
 
---
+++ src/main/java/net/kalangos/workshopmongo/repository/PostRepository.java
@@ -0,0 +1,11 @@
+package net.kalangos.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import net.kalangos.workshopmongo.domain.Post;
+
+@Repository
+public interface PostRepository extends MongoRepository<Post, String>{
+
+}
